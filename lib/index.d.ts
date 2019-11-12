6603bc7544791c52f88ecf17d1ca52e5b83a718b
Zachary Dean <zachary.dean@globallogic.com>
2019-11-11 16:30:26 -0800

    added export keyword

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -8,7 +8,7 @@ interface PromiseCache {
 }
 
 declare const PromiseCaches: PromiseCache[];
-declare const usePromise: (
+export declare const usePromise: (
   promise: (...inputs: any) => any,
   inputs: Array<any>,
   lifespan?: numbers,
